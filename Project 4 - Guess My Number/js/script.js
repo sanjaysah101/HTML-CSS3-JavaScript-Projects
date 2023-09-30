a88309a08c114b8a27c7ba03a710489cf4e4a586
Sanjay Kumar Sah <sanjay8797421521@gmail.com>
2023-09-30 16:11:48 +0530

    Build the guess the number game

---
+++ Project 4 - Guess My Number/js/script.js
@@ -0,0 +1,76 @@
+const $secretNumber = document.querySelector(".secretNumber");
+const $againBtn = document.querySelector("#again");
+const $messageLabel = document.querySelector(".message");
+const $checkResultBtn = document.querySelector("#check-result");
+const $scoreLabel = document.getElementById("score");
+const $highScoreLabel = document.getElementById("highscore");
+const $userGuessInput = document.getElementById("guess");
+
+let score = 20;
+let highScore = 0;
+let isGameOver = false;
+
+const generateNumber = () => Math.trunc(Math.random() * 20 + 1);
+const computeHighScore = currentScore => highScore = Math.max(highScore, currentScore);
+const displayMessage = message => $messageLabel.textContent = message;
+
+function resetGame() {
+    score = 20;
+    isGameOver = false;
+    secretNumber = generateNumber();
+    displayMessage("Start Guessing...");
+    $scoreLabel.textContent = score;
+    $highScoreLabel.textContent = highScore;
+    $userGuessInput.disabled = false;
+    $userGuessInput.focus();
+    $userGuessInput.classList.remove("disable");
+    $userGuessInput.placeholder = "";
+    $userGuessInput.value = "";
+    $secretNumber.textContent = "?";
+    $checkResultBtn.textContent = "Check!";
+}
+
+function disableUserInput(){
+    $userGuessInput.disabled = true;
+    $userGuessInput.classList.add("disable");
+    $userGuessInput.placeholder = "Game over";
+    $userGuessInput.value = "";
+    $checkResultBtn.textContent = "Over!";
+}
+
+let secretNumber = generateNumber();
+
+$againBtn.addEventListener("click", () => resetGame());
+
+$checkResultBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const userGuess = +$userGuessInput.value;
+    
+    if (score <= 0 || isGameOver) {
+        displayMessage("🚫 No Chance Left");
+        disableUserInput();
+        $secretNumber.textContent = secretNumber;
+        return;
+    }
+    if (!$userGuessInput.value) displayMessage("❌ invalid Number");
+    else if (userGuess > secretNumber) displayMessage("📈 Too High");
+    else if (userGuess < secretNumber) displayMessage("📉 Too Low");
+    else {
+        displayMessage("🎊 Correct Guess");
+        isGameOver = true;
+        computeHighScore(score);
+        disableUserInput();
+        $secretNumber.textContent = secretNumber;
+    }
+    
+    score--;
+    $scoreLabel.textContent = score;
+    $highScoreLabel.textContent = highScore;
+    // console.log(highScore);
+    
+    console.log(score);
+})
+
+
+
+
