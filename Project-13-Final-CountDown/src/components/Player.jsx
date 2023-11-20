53c52c8e2e3292a2a225d985bf6ef1ac3a67b008
Sanjay Kumar Sah <sanjay8797421521@gmail.com>
2023-11-20 11:38:11 +0530

    improve code

--- Project-13-Final-CountDown/src/components/Player.jsx
+++ Project-13-Final-CountDown/src/components/Player.jsx
@@ -3,7 +3,7 @@ import { useRef, useState } from "react";
 export default function Player() {
   const playerName = useRef();
 
-  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+  const [enteredPlayerName, setEnteredPlayerName] = useState("");
 
   const handleClick = () => {
     setEnteredPlayerName(playerName.current.value);
@@ -12,7 +12,7 @@ export default function Player() {
 
   return (
     <section id="player">
-      <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
+      <h2>Welcome {enteredPlayerName || "Anonymous"}</h2>
       <p>
         <input type="text" ref={playerName} />
         <button onClick={handleClick}>Set Name</button>
