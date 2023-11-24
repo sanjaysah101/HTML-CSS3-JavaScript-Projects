497721d1e6af2ca30c12ef6ff2115a4b4422574e
Sanjay Kumar Sah <sanjay8797421521@gmail.com>
2023-11-24 12:14:08 +0530

    getting input from form and add signup form

--- Project-17-React-Form/src/App.jsx
+++ Project-17-React-Form/src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header.jsx";
-import Login from "./components/Login.jsx";
+// import Login from "./components/Login.jsx";
+import Signup from "./components/Signup.jsx";
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <Login />
+        {/* <Login /> */}
+        <Signup />
       </main>
     </>
   );
--- Project-17-React-Form/src/components/Login.jsx
+++ Project-17-React-Form/src/components/Login.jsx
@@ -1,17 +1,52 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Reset Input Values
+    setEnteredValues({
+      email: "",
+      password: "",
+    });
+  };
+
+  const handleInputChange = (identifier, e) => {
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [identifier]: e.target.value,
+    }));
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(event) => handleInputChange("email", event)}
+            value={enteredValues.email}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(event) => handleInputChange("password", event)}
+            value={enteredValues.password}
+          />
         </div>
       </div>
 
